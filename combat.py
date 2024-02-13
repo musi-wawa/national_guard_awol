f27500053bcc4d65d8f45399250dbe47313f54c0
Fluidhelixtw <77405455+Fluidhelixtw@users.noreply.github.com>
2024-02-12 22:07:33 -0600

    dealing with a merge conflict

---
+++ combat.py
@@ -0,0 +1,100 @@
+from names import getRandomName
+from defines import *
+import random
+
+def generateTestSquad(newSquad,newSquadSize):
+    for _ in range(newSquadSize):
+        newFighter = Fighter(getRandomName(),[1,0,0])
+        newFighter.weapon = Item(1)
+        newFighter.equipment = Item(3)
+        newFighter.gear = Item(2,5)
+        newFighter.uniform = Item(10)
+        newSquad.append(newFighter)
+    return(newSquad)
+
+def getTestSquadSize():
+    validNumber = False    
+    squadSize = 0
+    while validNumber != True:
+        try:
+            squadSize = int(input("Size of squad? (1-10) "))
+            if squadSize > 0 and squadSize <= 10:
+                validNumber = True
+            else:
+                raise()
+        except:
+            print("Invalid input. Please enter a number from 1-10.")
+    return squadSize
+
+def battle(playerSide,computerSide,distance):
+
+    while True:
+        playerTurn = True
+        actionsLeft = 2
+                      #new player turn
+        while playerTurn == True:
+            clear()
+            narrateScene(playerSide,computerSide,distance)
+            showOptions(actionsLeft)
+            if actionsLeft == 0:
+                playerTurn = False
+                continue 
+            choice = input("\n   What do you do? ")
+            match choice.lower():
+                case "1" | "advance":
+                    if distance > 0:
+                        distance -= 1
+                        actionsLeft -=1
+                    else:
+                        print("Cannot advance any more!")
+                case "2" | "retreat":
+                    distance += 1
+                    actionsLeft -=1
+                case "3" | "attack":
+                    actionsLeft -=1
+                    attack(distance,playerSide,computerSide)
+                case "4" | "suppress":
+                    actionsLeft -=1
+                    attack(distance,playerSide,computerSide,True)
+                case 5 | "equipment":
+                    print("Equipment not added to the game yet. Sorry.")
+                case "6" | "inventory":
+                    viewInventoryCombat(playerSide)
+                case "7" | "examine":
+                    print()
+                case "8" | "pass" | "pass turn":
+                    actionsLeft -= 1
+
+def attack(distance,attackingSide,defendingSide,suppression = False):
+    if suppression == False:
+        for fighter in attackingSide.members:
+            if fighter.inventory.weapon.weapon["range"] <= distance and hasAmmo(fighter,attackingSide) == True:
+                print("bang")
+
+def hasAmmo(fighter,squad):
+    ammoNeeded = fighter.inventory.weapon.ammo
+    return True
+
+def viewInventoryCombat(squad): #doing things would cost actions if we were tracking actions per fighter instead of per squad
+    print()
+    print(squad)
+        
+
+def showOptions(actionsLeft):
+    if actionsLeft == 1:
+        print("It's your turn. You have 1 action left. Here are your options:")
+    else:
+        print("It's your turn. You have 2 actions left. Here are your options:")
+    print("1. Advance")
+    print("2. Retreat")
+    print("3. Attack")
+    print("4. Suppress")
+    print("5. Equipment")
+    print("6. Inventory")
+    print("7. Examine")
+    print("8. Pass turn")
+    print("Remember that you can do 'help' to learn about any command.")
+
+def narrateScene(yourSide,enemySide,distance):
+    print(f"Your side has a size of {len(yourSide.members)} and the enemy has a size of {len(enemySide.members)}.")
+    print(f"The enemy is {distance} tiles away from you")
--- defines.py
+++ defines.py
@@ -1,5 +1,10 @@
 import os
 import random
+import json
+
+with open("data/items.json") as itemDataFile:
+    itemData = json.load(itemDataFile)
+
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -16,7 +21,7 @@ class Squad:
 
 
 class Fighter:
-    def __init__(self, name = "John Doe", skills = [0,0,0]):
+    def __init__(self, name = "John Doe", skills = [0,0,0]): 
         self.name = name
         self.flavor = "A generic human being."
         self.weapon = Item(0)
@@ -25,10 +30,11 @@ class Fighter:
         self.headgear = Item(-1)
         self.storage = Item(-1)
         self.uniform = Item(-2)
-        self.skills = skills
+        self.skills = skills #marksmanship,melee,communication,medical,repair,animal,cooking
         self.actions = 0
-        self.morale = 3      #number is a placeholder for combat test
-        self.health = 4
+        self.discipline = 0
+        self.morale = self.discipline + 3 #discipline directly impacts maximum morale
+        self.health = 100
     
     def __str__(self):
         return f"{self.name}, {self.flavor}"
@@ -45,79 +51,16 @@ class Fighter:
         else:
             self.actions -= 1
 
-"""class Inventory:
-    def __init__(self, weapon = None, equipment = None, gear = None, storage = None, uniform = None):
-        if weapon == None:
-            self.weapon = Item(0)       #if new inventory slots are empty, create item 0 AKA "empty" in that slot
-        else: self.weapon = weapon
-        if equipment== None:
-            self.equipment = Item(-1)
-        else: self.equipment = equipment
-        if gear == None:
-            self.gear = Item(-1)
-        else: self.gear = gear
-        if storage == None:
-            self.storage = [Item(-1)]
-        if uniform == None:
-            self.uniform = Item(-2)"""
-
-    """def __str__(self):
-        return f"{self.weapon}, {self.equipment}, {self.gear}, {self.storage}"""
 class Item:
-    def __init__(self, id = 0, stack = 1):
+    def __init__(self, id="0", stack=1):
         self.id = id
         self.stack = stack
-        self.weapon = None
-        self.maxStack = 1
-        self.utility = None
-        self.droppable = True
-        match id:
-            case -2:
-                self.name = "nothing"
-                self.droppable = False
-                self.maxStack = 0
-                self.flavor = "This person is not wearing any clothing whatsoever. Not that anyone cares about that sort of thing anymore."
-                self.slots = ["uniform"]
-            case -1:
-                self.name = "empty"
-                self.droppable = False
-                self.maxStack = 0
-                self.flavor = "This slot is empty. There's nothing here."
-                self.slots = ["gear","equipment"]
-            case 0:
-                self.name = "unarmed"
-                self.droppable = False
-                self.flavor = "Hand to hand, fisticuffs, close quarters combat, martial arts, whatever you wanna call it. This person is entirely unarmed and is relying purely on their body for offense."
-                self.maxStack = 0
-                self.weapon = {"range":0,"damage":1,"cost":0,"suppress":0,"ammo":0,"bonus":-1}
-                self.slots = ["weapon"] 
-            case 1:
-                self.name = "modern assault rifle"
-                self.flavor = "A standard issue NG14 Assault Weapon, capable of holding 33 rounds of 7.79 HP in each external magazine. The magazine is loaded directly into the chamber so there is no 'plus one'. It is in a bullpup configuration and the magazines are quite bulky."
-                self.weapon = {"range":4,"damage":3,"cost":2,"suppress":4,"ammo":4,"bonus":3}
-                self.slots = ["weapon"]
-            case 2:
-                self.name = "modern marksman rifle"
-                self.flavor = "The NG20 Reconaissance Weapon, A semi-automatic marksman rifle firing from a small side-fed magazine, integrated irons, and a 10x scope. Using 7.79 High Penetration in the reverse configuration, this weapon smashes through armor like it’s nothing but is ineffective against unarmored opponents. Prototypes for 12.79 HP variants exist, but never saw use due to the scarcity of armored targets to shoot at that need much firepower."
-                self.weapon = {"range":6,"damage":2,"cost":1,"suppress":0,"ammo":4,"bonus":2}
-                self.slots = ["weapon"]
-            case 3:
-                self.name = "modern machine gun"
-                self.flavor = "A bullpup NG22 Suppression Weapon, holding an impressive 99 rounds. It is extremely bulky and has awful ergonomics, but more than makes up for it with its rate of fire and sheer power. It doesn’t look anything like machine guns of the past wars and the magazine is a horizontal brick above the trigger. Chambered in 7.79 HP."
-                self.weapon = {"range":4,"damage":4,"cost":4,"suppress":8,"ammo":4,"bonus":4}
-                self.slots = ["weapon"]
-            case 4:
-                self.name = "modern military rounds"
-                self.flavor = "A handful of 7.79 High Penetration rounds. Being a caseless round, they are dull green and rectangular. For long-range use, you can flip them around and they remain effective. Impossible to hand-load."
-                self.maxStack = 16
-                self.slots = ["equipment"]
-            case 5:
-                self.name = "tactical armor"
-            case 6:
-                self.name = "national guard field uniform"
-                self.flavor = "The standard issue field uniform for all National Guard troopers in Bastille. With a brodie helmet, red bandana and camouflauged fatigues, it's vaguely comfortable and fits pretty much no one. At least it's warm."
-                self.maxStack = 5
-                self.slots = ["uniform"]
+        data = itemData.get(self.id, {})
+        self.flavor = data.get("flavor", None)
+        self.maxStack = data.get("maxStack", None)
+        self.droppable = data.get("droppable", None)
+        self.stats = data.get("stats", None)
+        self.slots = data.get("slots", None)
 
-        if self.stack > self.maxStack:
+        if self.maxStack is not None and self.stack > self.maxStack:
             self.stack = self.maxStack  #corrects stack size if its higher than max size at init
